14fa3a69e5290afb7e7f6218ae292b6d898597ab
jula <122871245+jmahanera@users.noreply.github.com>
2023-11-19 17:34:26 +0100

    first commit

---
+++ src/components/CitySearch.js
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from "react";
+
+
+const CitySearch = ({ allLocations, setCurrentCity }) => {
+  const [showSuggestions, setShowSuggestions] = useState(false);
+  const [query, setQuery] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
+
+  // useEffect to update suggestions when allLocations changes
+  useEffect(() => {
+    setSuggestions(allLocations);
+  }, [allLocations]);
+
+  const handleInputChanged = (event) => {
+    const value = event.target.value;
+    const filteredLocations = allLocations
+      ? allLocations.filter((location) =>
+        location.toUpperCase().indexOf(value.toUpperCase()) > -1
+      )
+      : [];
+
+    setQuery(value);
+    setSuggestions(filteredLocations);
+  };
+
+const handleItemClicked = (event) => {
+    const value = event.target.textContent;
+    setQuery(value);
+    setShowSuggestions(false);
+    setCurrentCity(value);
+  };
+
+  return (
+    <div id="city-search">
+      <input
+        type="text"
+        className="city"
+        placeholder="Search for a city"
+        value={query}
+        onFocus={() => setShowSuggestions(true)}
+        onChange={handleInputChanged}
+      />
+      {showSuggestions ?
+        <ul className="suggestions">
+          {suggestions.map((suggestion) => (
+            <li onClick={handleItemClicked} key={suggestion}>{suggestion}</li>
+          ))}
+          <li key='See all cities' onClick={handleItemClicked}>
+            <b>See all cities</b>
+          </li>
+        </ul>
+        : null
+      }
+    </div>
+  );
+};
+
+
+
+export default CitySearch;
