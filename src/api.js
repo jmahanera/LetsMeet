c7cdaf11ab0c74cb0da9c02c314a24ae468ca030
jula <122871245+jmahanera@users.noreply.github.com>
2023-11-21 19:54:12 +0100

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,13 +1,5 @@
 import mockData from './mock-data';
 
-/**
- *
- * @param {*} events:
- * The following function should be in the “api.js” file.
- * This function takes an events array, then uses map to create a new array with only locations.
- * It will also remove all duplicates by creating another new array using the spread operator and spreading a Set.
- * The Set will remove all duplicates from the array.
- */
 export const extractLocations = (events) => {
   const extractedLocations = events.map((event) => event.location);
   const locations = [...new Set(extractedLocations)];
@@ -15,82 +7,128 @@ export const extractLocations = (events) => {
 };
 
 const checkToken = async (accessToken) => {
-  const response = await fetch(
-    `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
-  );
-  const result = await response.json();
-  return result;
+  try {
+    const response = await fetch(
+      `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
+    );
+
+    if (!response.ok) {
+      throw new Error('Token check failed');
+    }
+
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    console.error('Error checking token:', error);
+    throw error;
+  }
 };
 
 const removeQuery = () => {
-  let newurl;
-  if (window.history.pushState && window.location.pathname) {
-    newurl =
-      window.location.protocol +
-      "//" +
-      window.location.host +
-      window.location.pathname;
-    window.history.pushState("", "", newurl);
-  } else {
-    newurl = window.location.protocol + "//" + window.location.host;
-    window.history.pushState("", "", newurl);
+  try {
+    let newurl;
+    if (window.history.pushState && window.location.pathname) {
+      newurl =
+        window.location.protocol +
+        "//" +
+        window.location.host +
+        window.location.pathname;
+      window.history.pushState("", "", newurl);
+    } else {
+      newurl = window.location.protocol + "//" + window.location.host;
+      window.history.pushState("", "", newurl);
+    }
+  } catch (error) {
+    console.error('Error removing query:', error);
+    throw error;
   }
 };
 
 const getToken = async (code) => {
-  const encodeCode = encodeURIComponent(code);
-  const response = await fetch(
-    'https://a0u7kwigrc.execute-api.eu-central-1.amazonaws.com/dev/api' + '/' + encodeCode
-  );
-  const { access_token } = await response.json();
-  access_token && localStorage.setItem("access_token", access_token);
-  return access_token;
+  try {
+    const encodeCode = encodeURIComponent(code);
+    const response = await fetch(
+      'https://a0u7kwigrc.execute-api.eu-central-1.amazonaws.com/dev/api' + '/' + encodeCode
+    );
+
+    if (!response.ok) {
+      throw new Error('Token retrieval failed');
+    }
+
+    const { access_token } = await response.json();
+    access_token && localStorage.setItem("access_token", access_token);
+    return access_token;
+  } catch (error) {
+    console.error('Error getting token:', error);
+    throw error;
+  }
 };
-
-
-
 
 export const getAccessToken = async () => {
-  const accessToken = localStorage.getItem('access_token');
-  const tokenCheck = accessToken && (await checkToken(accessToken));
+  try {
+    const accessToken = localStorage.getItem('access_token');
+    const tokenCheck = accessToken && (await checkToken(accessToken));
 
-  if (!accessToken || tokenCheck.error) {
-    await localStorage.removeItem("access_token");
-    const searchParams = new URLSearchParams(window.location.search);
-    const code = await searchParams.get("code");
-    if (!code) {
-      const response = await fetch(
-        "https://a0u7kwigrc.execute-api.eu-central-1.amazonaws.com/dev/api/get-auth-url"
-      );
-      const result = await response.json();
-      const { authUrl } = result;
-      return (window.location.href = authUrl);
+    if (!accessToken || tokenCheck.error) {
+      await localStorage.removeItem("access_token");
+      const searchParams = new URLSearchParams(window.location.search);
+      const code = await searchParams.get("code");
+
+      if (!code) {
+        const response = await fetch(
+          "https://a0u7kwigrc.execute-api.eu-central-1.amazonaws.com/dev/api/get-auth-url"
+        );
+
+        if (!response.ok) {
+          throw new Error('Failed to get authentication URL');
+        }
+
+        const result = await response.json();
+        const { authUrl } = result;
+        return (window.location.href = authUrl);
+      }
+
+      return code && getToken(code);
     }
-    return code && getToken(code);
+
+    return accessToken;
+  } catch (error) {
+    console.error('Error getting access token:', error);
+    throw error;
   }
-  return accessToken;
 };
 
-
 export const getEvents = async () => {
-  if (window.location.href.startsWith('http://localhost')) {
-    return mockData;
-  }
-
-  const token = await getAccessToken();
-
-  if (token) {
-    removeQuery();
-    const url =
-      "https://a0u7kwigrc.execute-api.eu-central-1.amazonaws.com/dev/api/get-events" +
-      "/" +
-      token;
-    const response = await fetch(url);
-    const result = await response.json();
-    if (result) {
-      return result.events;
-    } else {
-      return null;
+  try {
+    if (window.location.href.startsWith('http://localhost')) {
+      return mockData;
     }
+
+    const token = await getAccessToken();
+
+    if (token) {
+      removeQuery();
+      const url =
+        "https://a0u7kwigrc.execute-api.eu-central-1.amazonaws.com/dev/api/get-events" +
+        "/" +
+        token;
+
+      const response = await fetch(url);
+
+      if (!response.ok) {
+        throw new Error('Failed to get events');
+      }
+
+      const result = await response.json();
+
+      if (result) {
+        return result.events;
+      } else {
+        return null;
+      }
+    }
+  } catch (error) {
+    console.error('Error getting events:', error);
+    throw error;
   }
 };
