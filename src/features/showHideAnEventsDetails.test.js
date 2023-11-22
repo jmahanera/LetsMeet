2b3eb9a3ba6f1a6877f2c5c9aba7ed71cde703d8
jula <122871245+jmahanera@users.noreply.github.com>
2023-11-22 23:33:52 +0100

    Update showHideAnEventsDetails.test.js

--- src/features/showHideAnEventsDetails.test.js
+++ src/features/showHideAnEventsDetails.test.js
@@ -1,122 +1,103 @@
 import { loadFeature, defineFeature } from 'jest-cucumber';
-import { render, within, waitFor } from '@testing-library/react';
 import App from '../App';
+import { render, waitFor, within } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 const feature = loadFeature('./src/features/showHideAnEventsDetails.feature');
 
+
 defineFeature(feature, (test) => {
-  test('An event element is collapsed by default', ({ given, when, then }) => {
+  //SCENARIO 1
+  test('An event element is collapsed by default.', ({ given, when, then }) => {
     let AppComponent;
-
-    given('the main page is open', () => {
+    given('the user first opens the app', () => {
       AppComponent = render(<App />);
     });
 
-    when('the user opens the app', () => {
-      // No additional action needed for this step
-    });
+    when(
+      'the user recieves the full list of events (specific for the city or all events)',
+      async () => {
+        const AppDOM = AppComponent.container.firstChild;
+        const EventListDOM = AppDOM.querySelector('#event-list');
 
-    then('the user should see the list of all upcoming events', async () => {
-      const AppDOM = AppComponent.container.firstChild;
-      const EventListDOM = AppDOM.querySelector('#event-list');
-      await waitFor(() => {
-        const EventListItems = within(EventListDOM).queryAllByRole('listitem');
-        expect(EventListItems.length).toBeGreaterThan(0);
-      });
-    });
+        await waitFor(() => {
+          const EventListItems =
+            within(EventListDOM).queryAllByRole('listitem');
+          expect(EventListItems.length).toBe(32);
+        });
+      }
+    );
 
-    then('an event element is collapsed by default', () => {
-      // Add your assertions for the default collapsed state
-      const AppDOM = AppComponent.container.firstChild;
-      const eventDetails = within(AppDOM).query('.event .details');
-      expect(eventDetails).toBeNull();
+    then('all events will colapse by default.', () => {
+      const EventDOM = AppComponent.container.firstChild;
+      const details = EventDOM.querySelector('.details');
+      expect(details).not.toBeInTheDocument();
     });
   });
 
-  test('User can expand an event to see its details', ({ given, when, then }) => {
+  // SCENARIO 2
+  test('User can expand an event to see its details', ({
+    given,
+    when,
+    then,
+  }) => {
     let AppComponent;
-    let AppDOM;
-
-    given('the main page is open', () => {
+    given('the user gets a list of events', async () => {
       AppComponent = render(<App />);
-    });
-
-    when('the user starts typing in the city textbox', async () => {
-      const user = userEvent.setup();
       const AppDOM = AppComponent.container.firstChild;
-      const CitySearchDOM = AppDOM.querySelector('#city-search');
-      const citySearchInput = within(CitySearchDOM).queryByRole('textbox');
-      await user.type(citySearchInput, 'Berlin');
-    });
 
-    when('the user selects a city (e.g., "Berlin, Germany") from the list', async () => {
-      const user = userEvent.setup();
-      const suggestionListItems = within(AppDOM).queryAllByRole('listitem');
-      await user.click(suggestionListItems[0]);
-    });
-
-    then('their city should be changed to that city (i.e., "Berlin, Germany")', () => {
-      const citySearchInput = within(AppDOM).queryByRole('textbox');
-      expect(citySearchInput.value).toBe('Berlin, Germany');
-    });
-
-    then('the user should receive a list of upcoming events in that city', async () => {
-      const EventListDOM = AppDOM.querySelector('#event-list');
       await waitFor(() => {
-        const EventListItems = within(EventListDOM).queryAllByRole('listitem');
-        expect(EventListItems.length).toBeGreaterThan(0);
-        // Add more assertions based on your application logic
+        const eventList = within(AppDOM).queryAllByRole('listitem');
+        expect(eventList[0]).toBeTruthy();
       });
     });
 
-    then('the user can expand an event to see its details', () => {
-      // Add your assertions for the ability to expand an event
-      const eventDetails = within(AppDOM).query('.event .details');
-      expect(eventDetails).toBeDefined();
+    when("a user selects an event's details", async () => {
+      const button = AppComponent.queryAllByText('Show Details')[0];
+
+      await userEvent.click(button);
+    });
+
+    then('the details will show up for that choosen event', () => {
+      const EventDOM = AppComponent.container.firstChild;
+      const details = EventDOM.querySelector('.details');
+      expect(details).toBeInTheDocument();
     });
   });
 
-  test('User can collapse an event to hide details', ({ given, when, then }) => {
+  // SCENARIO 3
+  test('User can collapse an event to hide its details', ({
+    given,
+    when,
+    then,
+  }) => {
     let AppComponent;
-    let AppDOM;
-
-    given('the main page is open', () => {
+    let button;
+    given('the user sees the details of an event', async () => {
       AppComponent = render(<App />);
-    });
+      const AppDOM = AppComponent.container.firstChild;
 
-    when('the user starts typing in the city textbox', async () => {
-      const user = userEvent.setup();
-      AppDOM = AppComponent.container.firstChild;
-      const CitySearchDOM = AppDOM.querySelector('#city-search');
-      const citySearchInput = within(CitySearchDOM).queryByRole('textbox');
-      await user.type(citySearchInput, 'Berlin');
-    });
-
-    when('the user selects a city (e.g., "Berlin, Germany") from the list', async () => {
-      const user = userEvent.setup();
-      const suggestionListItems = within(AppDOM).queryAllByRole('listitem');
-      await user.click(suggestionListItems[0]);
-    });
-
-    when('the user can expand an event to see its details', () => {
-      // Assuming there's an action to expand an event
-      const eventDetailsButton = within(AppDOM).query('.event .details-btn');
-      userEvent.click(eventDetailsButton);
-    });
-
-    when('the user collapses the event', () => {
-      // Assuming there's an action to collapse an event
-      const eventDetailsButton = within(AppDOM).query('.event .details-btn');
-      userEvent.click(eventDetailsButton);
-    });
-
-    then('the event details should be hidden', async () => {
-      const EventListDOM = AppDOM.querySelector('#event-list');
       await waitFor(() => {
-        const eventDetails = within(EventListDOM).query('.event .details');
-        expect(eventDetails).toBeNull();
+        const eventList = within(AppDOM).queryAllByRole('listitem');
+        expect(eventList[0]).toBeTruthy();
       });
+
+      button = AppComponent.queryAllByText('Show Details')[0];
+      await userEvent.click(button);
+
+      const EventDOM = AppComponent.container.firstChild;
+      const details = EventDOM.querySelector('.details');
+      expect(details).toBeInTheDocument();
+    });
+
+    when("the user presses a button to hide event's details", async () => {
+      await userEvent.click(button);
+    });
+
+    then('the details of that even will be hidden', () => {
+      const EventDOM = AppComponent.container.firstChild;
+      const details = EventDOM.querySelector('.details');
+      expect(details).not.toBeInTheDocument();
     });
   });
 });
