312629e7a50ad14aaee3ee2fe121c16874674847
jula <122871245+jmahanera@users.noreply.github.com>
2023-11-22 09:02:32 +0100

    Update NumberOfEvents.js

--- src/components/NumberOfEvents.js
+++ src/components/NumberOfEvents.js
@@ -4,7 +4,7 @@ import { useState } from "react";
 
 const NumberOfEvents = ({ }) => {
 
-  const [number, setNumber] = useState(10);
+  const [number, setNumber] = useState(32);
 
   const handleInputChanged = (event) => {
     const value = event.target.value;
