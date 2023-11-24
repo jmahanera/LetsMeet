05b444d3df51b06446268cf5134be18a30796359
jula <122871245+jmahanera@users.noreply.github.com>
2023-11-24 18:02:05 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ const App = () => {
   }, [currentCity, currentNOE]);
 
   const fetchData = async () => {
-    const allEvents = getEvents();
+    const allEvents = await getEvents();
     const filteredEvents = currentCity === "See all cities" ?
       allEvents :
       allEvents.filter(event => event.location === currentCity)
