32655a773346cd9a152e77662dd306800760812e
jula <122871245+jmahanera@users.noreply.github.com>
2023-11-29 12:24:36 +0100

    chart updated

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { useEffect, useState } from 'react';
 import { extractLocations, getEvents } from './api';
 import { InfoAlert, ErrorAlert } from './components/Alert';
 import { WarningAlert } from "./components/Alert";
+import CityEventsChart from "./components/CityEventsChart";
 
 import './App.css';
 
@@ -44,6 +45,7 @@ const App = () => {
         {errorAlert.length ? <ErrorAlert text={errorAlert} /> : null}
         {warningAlert.length ? <WarningAlert text={warningAlert} /> : null}
       </div>
+      
  <div className="title-box">
   <h1 className="title">
     LetsMeet <span role="img" aria-label="group" className="group-symbol">👥</span>
@@ -52,13 +54,7 @@ const App = () => {
     Search Tech Events around the World!
   </h3>
 </div>
-
-
-
-
-
-
-      <CitySearch
+ <CitySearch
         allLocations={allLocations}
         setCurrentCity={setCurrentCity}
         setInfoAlert={setInfoAlert}
@@ -68,6 +64,7 @@ const App = () => {
         setCurrentNOE={setCurrentNOE}
         setErrorAlert={setErrorAlert}
       />
+      <CityEventsChart allLocations={allLocations} events={events} />
       <EventList events={events} />
     </div>
   );
--- src/mock-data.js
+++ src/mock-data.js
@@ -592,8 +592,8 @@ const mockData = [
    "id": "3qtd6uscq4tsi6gc7nmmtpqlct_20200605T120000Z",
    "status": "confirmed",
    "htmlLink": "https://www.google.com/calendar/event?eid=M3F0ZDZ1c2NxNHRzaTZnYzdubW10cHFsY3RfMjAyMDA2MDVUMTIwMDAwWiBmdWxsc3RhY2t3ZWJkZXZAY2FyZWVyZm91bmRyeS5jb20",
-   "created": "2020-05-19T19:14:30.000Z",
-   "updated": "2020-05-27T11:45:37.792Z",
+   "created": "2023-05-19T19:14:30.000Z",
+   "updated": "2023-05-27T11:45:37.792Z",
    "summary": "React is Fun",
    "description": "Love HTML, CSS, and JS? Want to become a cool front-end developer? \n\nReact is one of the most popular front-end frameworks. There is a huge number of job openings for React developers in most cities. \n\nJoin us in our free React training sessions and give your career a new direction. ",
    "location": "Hannover, Germany",
@@ -606,16 +606,16 @@ const mockData = [
     "self": true
    },
    "start": {
-    "dateTime": "2020-06-05T14:00:00+02:00",
+    "dateTime": "2023-06-05T14:00:00+02:00",
     "timeZone": "Europe/Berlin"
    },
    "end": {
-    "dateTime": "2020-06-05T15:00:00+02:00",
+    "dateTime": "2023-06-05T15:00:00+02:00",
     "timeZone": "Europe/Berlin"
    },
    "recurringEventId": "3qtd6uscq4tsi6gc7nmmtpqlct",
    "originalStartTime": {
-    "dateTime": "2020-06-05T14:00:00+02:00",
+    "dateTime": "2023-06-05T14:00:00+02:00",
     "timeZone": "Europe/Berlin"
    },
    "iCalUID": "3qtd6uscq4tsi6gc7nmmtpqlct@google.com",
