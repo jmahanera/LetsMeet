772bfe61e48ab9bf4628b85d3a888d129319feb2
jula <122871245+jmahanera@users.noreply.github.com>
2023-11-29 22:14:22 +0100

    Update EventGenresChart.js

--- src/components/EventGenresChart.js
+++ src/components/EventGenresChart.js
@@ -1,60 +1,75 @@
 import React, { useEffect, useState } from "react";
 import { PieChart, Pie, Cell, ResponsiveContainer } from "recharts";
 
-const genres = ["React", "JavaScript", "Node", "jQuery", "Angular"];
-const colors = ["#DDC4DD", "#9F7CCB", "#A997DF", "#4F517D", "#1E1A3B"];
-
 const RADIAN = Math.PI / 180;
 
 const EventGenresChart = ({ events }) => {
-    const [data, setData] = useState([]);
+  const [data, setData] = useState([]);
+  const genres = ["React", "JavaScript", "Node", "jQuery", "Angular"];
 
-    useEffect(() => {
-        setData(getData());
-    }, [events]);
+  useEffect(() => {
+    setData(getData());
+  }, [events]);
 
-    const getData = () => {
-        const data = genres.map((genre) => {
-            const filteredEvents = events.filter((event) =>
-                event.summary.includes(genre)
-            );
-            return { name: genre, value: filteredEvents.length };
-        });
-        return data;
-    };
+  const getData = () => {
+    const data = genres.map((genre) => {
+      const filteredEvents = events.filter((event) =>
+        event.summary.includes(genre)
+      );
+      return { name: genre, value: filteredEvents.length };
+    });
+    return data;
+  };
 
-    const renderCustomizedLabel = ({ cx, cy, midAngle, outerRadius, percent, index }) => {
-        const radius = outerRadius;
-        const x = cx + radius * Math.cos(-midAngle * RADIAN) * 1.07;
-        const y = cy + radius * Math.sin(-midAngle * RADIAN) * 1.07;
-        return percent ? (
-            <text
-                x={x}
-                y={y}
-                fill="#8884d8"
-                textAnchor={x > cx ? 'start' : 'end'}
-                dominantBaseline="central"
-            >
-                {`${genres[index]} ${(percent * 100).toFixed(0)}%`}
-            </text>
-        ) : null;
-    };
+  const renderCustomizedLabel = ({
+    cx,
+    cy,
+    midAngle,
+    outerRadius,
+    percent,
+    index,
+  }) => {
+    const radius = outerRadius;
+    const x = cx + radius * Math.cos(-midAngle * RADIAN) * 1.07;
+    const y = cy + radius * Math.sin(-midAngle * RADIAN) * 1.07;
+    return percent ? (
+      <text
+        key={`label-${index}-${percent}`}  // Ensure unique key
+        x={x}
+        y={y}
+        fill="#8884d8"
+        textAnchor={x > cx ? "start" : "end"}
+        dominantBaseline="central"
+      >
+        {`${genres[index]} ${(percent * 100).toFixed(0)}%`}
+      </text>
+    ) : null;
+  };
 
-    return (
-        <div>
-            <ResponsiveContainer width="99%" height={400}>
-                <PieChart width={730} height={300}>
-  <Pie data={data} cx="50%" cy="50%" outerRadius={80} label>
-    {
-      data.map((entry, index) => (
-        <Cell key={`cell-${index}`} fill={colors[index]}/>
-      ))
-    }
-  </Pie>
-</PieChart>
-            </ResponsiveContainer>
-        </div>
-    );
+  const colors = ["#DDC4DD", "#9F7CCB", "#A997DF", "#4F517D", "#1E1A3B"];
+
+  return (
+    <div>
+      <p className="graph-title">Type of Event</p>
+
+      <ResponsiveContainer width="99%" height={400}>
+        <PieChart>
+          <Pie
+            data={data}
+            dataKey="value"
+            fill="#9F7CCB"
+            labelLine={false}
+            label={renderCustomizedLabel}
+            outerRadius={130}
+          >
+            {data.map((entry, index) => (
+              <Cell key={`cell-${index}`} fill={colors[index]} />
+            ))}
+          </Pie>
+        </PieChart>
+      </ResponsiveContainer>
+    </div>
+  );
 };
 
 export default EventGenresChart;
