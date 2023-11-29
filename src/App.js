01059f933347610a54ebe8f77fc33a0c312f6e3e
jula <122871245+jmahanera@users.noreply.github.com>
2023-11-29 14:10:53 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { extractLocations, getEvents } from './api';
 import { InfoAlert, ErrorAlert } from './components/Alert';
 import { WarningAlert } from "./components/Alert";
 import CityEventsChart from "./components/CityEventsChart";
+import EventGenresChart from "./components/EventGenresChart";
 
 import './App.css';
 
@@ -64,7 +65,10 @@ const App = () => {
         setCurrentNOE={setCurrentNOE}
         setErrorAlert={setErrorAlert}
       />
-      <CityEventsChart allLocations={allLocations} events={events} />
+       <div className="charts-container">
+        <EventGenresChart events={events} />
+        <CityEventsChart allLocations={allLocations} events={events} />
+      </div>
       <EventList events={events} />
     </div>
   );
--- src/tests/EndToEnd.test.js
+++ src/tests/EndToEnd.test.js
@@ -5,38 +5,66 @@ jest.setTimeout(30000); // Set a timeout for the entire test suite
 describe('show/hide event details', () => {
   let browser;
   let page;
+
   beforeAll(async () => {
-    browser = await puppeteer.launch({
-      headless: false,
-      slowMo: 250,
-      timeout: 0,
-    });
-    page = await browser.newPage();
-    await page.goto('http://localhost:3000/');
-    await page.waitForSelector('.event');
+  browser = await puppeteer.launch({
+    headless: false,
+    slowMo: 250,
   });
+  page = await browser.newPage();
+  await page.goto('http://localhost:3000/', { waitUntil: 'domcontentloaded' });
+  // Wait for the initial content to load
+  await page.waitForSelector('.event', { timeout: 30000 }); // Adjust the timeout value as needed
+});
+
 
   afterAll(async () => {
-    await browser.close();
+    // Ensure that the browser is open before attempting to close
+    if (browser) {
+      await browser.close();
+    }
   });
 
   // SCENARIO 1
   test('An event element is collapsed by default', async () => {
+    // Wait for the event selector in the context of this test
+    await page.waitForSelector('.event');
     const eventDetails = await page.$('.event .details');
     expect(eventDetails).toBeNull();
   });
 
   // SCENARIO 2
   test('User can expand an event to see its details', async () => {
-    await page.click('.event .details-btn');
+    // Wait for the event selector in the context of this test
+    await page.waitForSelector('.event');
+
+    const detailsButton = await page.$('.event .details-btn');
+    await detailsButton.click();
+
+    // Wait for a short time to allow the DOM to settle
+    await page.waitForTimeout(500);
+
+    // Wait for the details to appear
+    await page.waitForSelector('.event .details');
 
     const eventDetails = await page.$('.event .details');
-    expect(eventDetails).toBeDefined();
+    expect(eventDetails).toBeTruthy();
   });
 
   // SCENARIO 3
- test('User can collapse an event to hide details', async () => {
-    await page.click('.event .details-btn');
+  test('User can collapse an event to hide details', async () => {
+    // Wait for the event selector in the context of this test
+    await page.waitForSelector('.event');
+
+    const detailsButton = await page.$('.event .details-btn');
+    await detailsButton.click();
+
+    // Wait for a short time to allow the DOM to settle
+    await page.waitForTimeout(500);
+
+    // Wait for the details to disappear
+    await page.waitForSelector('.event .details', { hidden: true });
+
     const eventDetails = await page.$('.event .details');
     expect(eventDetails).toBeNull();
   });
